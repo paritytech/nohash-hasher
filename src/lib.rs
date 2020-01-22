d37ee35be523dfef91d8e605cd02cc26d47c4548
Pierre Krieger <pierre.krieger1708@gmail.com>
2020-01-22 10:24:41 +0100

    Fix concern

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,6 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
-#[cfg(feature = "std")]
-extern crate core;
-
 use core::{hash::{BuildHasherDefault, Hasher}, marker::PhantomData};
 
 /// A `HashMap` with an integer domain, using `NoHashHasher` to perform no hashing at all.
